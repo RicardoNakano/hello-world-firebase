2c6ce6f22b9ed6cd1351f86960fbef8c2645e7a1
RicardoNakano <nakanex@gmail.com>
2025-05-23 18:56:36 -0500

    Fixed CORS in Cloud Function and deployed Hosting

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require('firebase-functions');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
+  // Adiciona cabeçalhos CORS
+  response.set('Access-Control-Allow-Origin', '*'); // Permite todos os domínios (ou use 'https://hello-world-firebase-f1b5d.web.app' pra ser específico)
+  response.set('Access-Control-Allow-Methods', 'GET, POST'); // Métodos permitidos
+  response.set('Access-Control-Allow-Headers', 'Content-Type'); // Cabeçalhos permitidos
+
+  // Responde com "Hello World!"
   response.send('Hello World!');
 });
